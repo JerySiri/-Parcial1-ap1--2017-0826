d8e6945ad4d60898fc6ce094c1c214c9fc5a1f05
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-02-08 18:36:19 -0400

    Entidad ciudades

---
+++ Parcial1-ap1-2017-0826/Entidades/Ciudades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_ap1_2017_0826.Entidades
+{
+    class Ciudades
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
