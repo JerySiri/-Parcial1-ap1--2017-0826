e3c7153ae5648d0a15f5c8d950394a024332ddd0
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-02-08 18:50:31 -0400

    Clase Contexto

---
+++ Parcial1-ap1-2017-0826/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_ap1_2017_0826.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_ap1_2017_0826
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Ciudad> Ciudad{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Ciudades.db");
+        }
+    }
+}
