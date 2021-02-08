9b74669240f17b1857f0302127c859f82a3cc221
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-02-08 18:53:14 -0400

    Agrege Migracion

---
+++ Parcial1-ap1-2017-0826/Migrations/20210208225231_inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_ap1_2017_0826.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudad",
+                columns: table => new
+                {
+                    CiudadId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudad", x => x.CiudadId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudad");
+        }
+    }
+}
